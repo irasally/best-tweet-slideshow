3937d45bb54978db6300831f8b7cb2dc2f6d7f2d
irasally <irasally@gmail.com>
2013-12-07 02:08:36 +0900

    statusIdを使ってtwitter-apiにアクセスし、embedオブジェクトを取得する

---
+++ get-tweet-embed.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'json'
+File.open('data/tweetData.json', 'w').puts('[')
+
+idx = 0
+File.open('data/statusIds').each_line {|line|
+  id = line.strip
+  url = "https://api.twitter.com/1/statuses/oembed.json?id=#{id}"
+  p id
+  res = JSON.parse(open(url).read)
+  data = res["html"]
+  screenName = res["author_url"].scan(/.*\/twitter.com\/(\w+)/)[0][0]
+  embed = { :id => id.strip, :sreenName => screenName, :embedData => data}.to_json
+  if idx == 0
+    File.open('data/tweetData.json', 'a').puts(embed)
+  else
+    File.open('data/tweetData.json', 'a').puts( "," + embed)
+  end
+  idx = idx + 1
+}
+File.open('data/tweetData.json', 'a').puts(']')
