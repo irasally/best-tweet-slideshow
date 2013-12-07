80bf5279c085a4c047382fae0ef6ef2f5d65e530
irasally <irasally@gmail.com>
2013-12-07 13:53:10 +0900

    ファイル名は外部から指定するようにした

--- get-status-id.rb
+++ get-status-id.rb
@@ -1,5 +1,5 @@
 File.open('data/statusIds', 'w')
-File.open('/Users/irasally/Documents/bookmarks_13_12_07.html').each_line {|line|
+File.open(ARGV[0]).each_line {|line|
   res = line.scan(/.*\/status\/(\d+)/)
   File.open('data/statusIds', 'a').puts(res[0][0])
 }
